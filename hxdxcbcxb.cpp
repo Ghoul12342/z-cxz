b93423a64a0db9051ae9b6fdb3dc6d99e4aff56f
Ghoul12342 <114170053+Ghoul12342@users.noreply.github.com>
2022-10-27 18:10:12 +0300

    Add files via upload

---
+++ hxdxcbcxb.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+using namespace std;
+int main() {
+	/*int n;
+	cin >> n;
+		for (int i = 1; i <= n; i++) {
+			cout << "&" << " ";
+		}*/
+	/*int a, b;
+	cin >> a >> b;
+	int i = b;
+	while (i >= a) {
+		
+		cout << i << " ";
+		i--;
+	}*/
+	//for 2
+/*	for (int i = 20; i <= 30; i+=2) {
+		cout << i << " ";
+	}*/
+
+
+/*	int a, b;
+	cin >> a >> b;
+	int i = a;
+	int n = 0;
+	while (i <= b) {
+		if (i % 2 == 0) {
+			n = n + i;
+		}
+			
+			i++;
+	}
+	cout << n << " ";*/
+ //3
+	/*int n;
+	int pr = 1;
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		if (i % 5 != 0) {
+			pr = pr * i;
+		}
+	}
+	cout << pr << " ";*/
+	/*int n;
+	cin >> n;
+	double sym = 0;
+	double i = 1;
+	while (i<=n) {
+		sym = sym + 1 / i;
+		i++;
+	}
+	
+	cout << sym << " ";*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
